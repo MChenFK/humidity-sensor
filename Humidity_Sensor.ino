f6f221cee0d481fe46e34bb0feb3ee2ab4f3d6d9
Michael Chen <MichaelChen1923@gmail.com>
2025-05-20 16:26:07 -0400

    Add files via upload

--- Humidity_Sensor.ino
+++ Humidity_Sensor.ino
@@ -2,44 +2,83 @@
 #include <Wire.h>
 #include "Adafruit_SHT31.h"
 
+bool PLX_flag = true;
 bool enableHeater = false;
 uint8_t loopCnt = 0;
+
+char label1[] = "time (ms)";
+char label2[] = "temp (C)";
+char label3[] = "humidity (%)";
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 void setup() {
   Serial.begin(9600);
+  delay(10000);
 
   while (!Serial)
     delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
-  Serial.println("SHT31 test");
+  if (! PLX_flag){
+    Serial.println("SHT31 test");
+  }
   
   if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
-    Serial.println("Couldn't find SHT31");
+    if (! PLX_flag){
+      Serial.println("Couldn't find SHT31");
+    }
     while (1) delay(1);
   }
 
-  Serial.print("Heater Enabled State: ");
-  if (sht31.isHeaterEnabled())
-    Serial.println("ENABLED");
-  else
-    Serial.println("DISABLED");
+  if (! PLX_flag){
+      Serial.print("Heater Enabled State: ");
+    if (sht31.isHeaterEnabled())
+      Serial.println("ENABLED");
+    else
+      Serial.println("DISABLED");
+  }
+
+  if (PLX_flag) {
+    //char buffer[] = "";
+    //sprintf (buffer, "LABEL, %s, %s, %s", label1, label2, label3);
+    Serial.println("LABEL, seconds, temp (C), humidity (%)");
+  }
+  
 }
 
 
 void loop() {
+  if (PLX_flag) {
+    Serial.print("DATA, ");
+    Serial.print(millis() / 1000 - 10);
+    Serial.print(", ");
+  }
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
 
   if (! isnan(t)) {  // check if 'is not a number'
-    Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
+    if (! PLX_flag) {
+      Serial.print("Temp *C = ");
+    }
+    Serial.print(t); 
+    if (! PLX_flag) {
+      Serial.print("\t\t");
+    }
+    if (PLX_flag) {
+      Serial.print(", ");
+    }
   } else { 
     Serial.println("Failed to read temperature");
   }
   
   if (! isnan(h)) {  // check if 'is not a number'
-    Serial.print("Hum. % = "); Serial.println(h);
+    if (! PLX_flag) {
+      Serial.print("Hum. % = ");
+      Serial.println(h);
+    } else {
+      Serial.println(h);
+    }
+    
   } else { 
     Serial.println("Failed to read humidity");
   }
@@ -51,11 +90,13 @@ void loop() {
   if (loopCnt >= 30) {
     enableHeater = !enableHeater;
     sht31.heater(enableHeater);
-    Serial.print("Heater Enabled State: ");
-    if (sht31.isHeaterEnabled())
-      Serial.println("ENABLED");
-    else
-      Serial.println("DISABLED");
+    if (! PLX_flag) {
+      Serial.print("Heater Enabled State: ");
+      if (sht31.isHeaterEnabled())
+        Serial.println("ENABLED");
+      else
+        Serial.println("DISABLED");
+    }
 
     loopCnt = 0;
   }
